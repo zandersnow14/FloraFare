39ff7e97e7dbbddfd019e83717d499e7475cee33
Zander Snow <trainee.zander.snow@sigmalabs.co.uk>
2024-04-25 16:37:33 +0100

    Added login manager

--- website/__init__.py
+++ website/__init__.py
@@ -3,9 +3,10 @@
 from os import _Environ
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
-from .views import views
-from .auth import auth
+db = SQLAlchemy()
 
 
 def create_app(config: _Environ):
@@ -13,6 +14,22 @@ def create_app(config: _Environ):
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = config['SECRET_KEY']
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://zandersnow@localhost/flora_fare'
+    db.init_app(app)
+
+    from .models import User
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.filter_by(user_id=user_id).first()
+
+    from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
