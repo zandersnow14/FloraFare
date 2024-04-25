eb9c0155844b31b66b70981defdb99d555f197cb
Zander Snow <trainee.zander.snow@sigmalabs.co.uk>
2024-04-25 16:39:12 +0100

    Secured home page

--- website/views.py
+++ website/views.py
@@ -1,10 +1,12 @@
 """Routes for the website."""
 
 from flask import Blueprint, render_template
+from flask_login import login_required
 
 views = Blueprint('views', __name__)
 
 
 @views.route("/")
+@login_required
 def home():
     return render_template('home.html')
