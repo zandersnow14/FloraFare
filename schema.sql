183b70b06e97c3d910ef6418021835cd8fe806b8
Zander Snow <trainee.zander.snow@sigmalabs.co.uk>
2024-04-29 16:24:37 +0100

    Added price and sub tables

--- schema.sql
+++ schema.sql
@@ -14,8 +14,23 @@ CREATE TABLE plants (
     plant_id SERIAL PRIMARY KEY,
     plant_name TEXT UNIQUE NOT NULL,
     original_price DECIMAL,
-    curr_price DECIMAL,
     url TEXT UNIQUE NOT NULL,
     image_url TEXT UNIQUE NOT NULL,
     in_stock BOOLEAN
+);
+
+CREATE TABLE subscriptions (
+    sub_id SERIAL PRIMARY KEY,
+    user_id INT,
+    plant_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (plant_id) REFERENCES plants(plant_id)
+);
+
+CREATE TABLE prices (
+    price_id SERIAL PRIMARY KEY,
+    price DECIMAL NOT NULL,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    plant_id INT,
+    FOREIGN KEY (plant_id) REFERENCES plants(plant_id)
 )
