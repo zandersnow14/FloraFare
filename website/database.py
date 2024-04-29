cd5ebc4f469205c5fc65e0d7c2a7b4a6ed73c9ac
Zander Snow <trainee.zander.snow@sigmalabs.co.uk>
2024-04-29 16:25:09 +0100

    Added functions to enter and retrieve plant data

--- website/database.py
+++ website/database.py
@@ -4,6 +4,7 @@ from os import _Environ, environ
 from dotenv import load_dotenv
 from psycopg2 import connect
 from psycopg2.extensions import connection
+from psycopg2.extras import RealDictCursor, RealDictRow
 
 
 def get_db_conn(config: _Environ) -> connection:
@@ -50,10 +51,67 @@ def get_hashpw(conn: connection, email: str) -> str:
         return hashpw
 
 
+def insert_plant_data(db_conn: connection, plant_data: dict) -> None:
+    """Inserts plant data into the database."""
+
+    with db_conn.cursor() as cur:
+        cur.execute(
+            """INSERT INTO plants(plant_name, original_price, url, image_url, in_stock) VALUES (%s, %s, %s, %s, %s);""",
+            (plant_data['plant_name'], plant_data['og_price'], plant_data['url'], plant_data['image_url'], plant_data['in_stock']))
+        db_conn.commit()
+
+
+def insert_subscription(db_conn: connection, plant_data: dict, user_id: int) -> None:
+    """Inserts a subscription into the database."""
+
+    with db_conn.cursor() as cur:
+        cur.execute("""SELECT plant_id FROM plants WHERE plant_name = (%s);""",
+                    (plant_data['plant_name'],))
+        plant_id = cur.fetchone()[0]
+
+        cur.execute(
+            """INSERT INTO subscriptions(user_id, plant_id) VALUES (%s, %s);""", (user_id, plant_id))
+
+        db_conn.commit()
+
+
+def is_plant_in_db(db_conn: connection, plant_data: dict) -> bool:
+    """Checks whether a plant is already in the database."""
+
+    with db_conn.cursor() as cur:
+        cur.execute("""SELECT * FROM plants WHERE plant_name = (%s);""",
+                    (plant_data['plant_name'],))
+
+        results = cur.fetchall()
+
+        if results:
+            return True
+        return False
+
+
+def get_user_plants(db_conn: connection, user_id: int) -> list[RealDictRow]:
+    """Returns all the plant data for a given user."""
+
+    with db_conn.cursor(cursor_factory=RealDictCursor) as cur:
+
+        cur.execute(
+            """SELECT plants.* FROM plants JOIN subscriptions AS sub ON sub.plant_id = plants.plant_id WHERE sub.user_id = (%s);""",
+            (user_id,))
+        results = cur.fetchall()
+
+        return results
+
+
 if __name__ == "__main__":
 
     load_dotenv()
 
     conn = get_db_conn(environ)
 
-    print(is_email_taken(conn, "zandersnow144@gmail.com"))
+    plant_data = {'plant_name': "test_plant", 'og_price': 12.68,
+                  'url': 'test_url', 'image_url': 'test_image', 'in_stock': True}
+
+    plants = get_user_plants(conn, 1)
+
+    for plant in plants:
+        print(plant['plant_name'])
